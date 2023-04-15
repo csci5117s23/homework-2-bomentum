2e841cb501a521826e36dea8bd9b21af94faf7f1
bomentum <55463846+bomentum@users.noreply.github.com>
2023-04-15 13:58:53 -0500

    key

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     env:{
     NEXT_COHO_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY
+    
   }
 };
 
