e842fcce5dd3144bf963337f6c2de7672bd5186a
bomentum <55463846+bomentum@users.noreply.github.com>
2023-04-17 02:04:55 -0500

    donebutton build error

--- src/pages/todo/[id].js
+++ src/pages/todo/[id].js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useAuth } from '@clerk/clerk-react';
 import { oneItem, updateItem } from "@/modules/data";
 import { useState, useEffect } from 'react';
-import { DoneButton } from "../doneButton";
+import { DoneButton } from "../../components/donebutton";
 
 
 export default function Id() {
